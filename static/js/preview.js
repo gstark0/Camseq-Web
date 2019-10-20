90a929da5198ff3cd27f1f5a5c86608c74477c38
gstark0 <gstark0@icloud.com>
2019-10-20 15:45:57 +0200

    Camera footage preview

--- static/js/preview.js
+++ static/js/preview.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     updateTime();
+    updateImage();
     $('#camera-name').text(cameraInfo['name']);
     $('#location').text(cameraInfo['location']);
     $('#coordinates').text(cameraInfo['coordinates']);
@@ -30,14 +31,24 @@ function updateTime() {
     let today = new Date();
     let hours = today.getHours();
     let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
     minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
 
     let day = checkTime(today.getDate());
     let month = checkTime(today.getMonth()+1);
     let year = today.getFullYear();
 
-    $('#time').text(day + '/' + month + '/' + year + ' - ' + hours + ':' + minutes);
-    let t = setTimeout(updateTime, 500);
+    $('#time').text(day + '/' + month + '/' + year + ' - ' + hours + ':' + minutes + ':' + seconds);
+
+    let t = setTimeout(updateTime, 1000);
+}
+
+function updateImage() {
+    let imgUrl = cameraInfo['url'] + '?r=' + new Date().getTime();
+    $('#camera-image').css('background-image', 'url("' + imgUrl + '")');
+
+    let t = setTimeout(updateImage, 10000)
 }
 
 function checkTime(i) {
